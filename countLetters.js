2e2366c80ca64de21919528d9aae5f9265246ed9
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-03 15:56:22 -0800

    Add files via upload

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let result = {};
+  let charArray = sentence.split("");
+  for (let i = 0; i < charArray.length; i++) {
+    if (charArray[i] === " ") {
+      charArray.splice(i, 1);
+    }
+  }
+  for (let i = 0; i < charArray.length; i++) {
+    result[charArray[i]] = 0;
+  }
+  for (let i = 0; i < charArray.length; i++) {
+    result[charArray[i]] += 1;
+  }
+  return result;
+}
+
+console.log(countLetters("lighthouse in the house"));
