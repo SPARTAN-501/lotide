6c7f895d55bffdebd219bcb2c6468d7cd1ac9b5b
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-03 18:25:35 -0800

    Add files via upload

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let result = undefined;
+  let keysList = Object.keys(obj);
+  for (let i = 0; i < keysList.length; i++) {
+    let key = keysList[i];
+    if (obj[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
