94bf6f5b8102b724588fd84c83f59be636a918bc
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-04 16:55:50 -0800

    Add files via upload

---
+++ map.js
@@ -0,0 +1,35 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const eqArrays = function(arr1, arr2) {
+  let truthy = false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      truthy = true;
+    }
+    else {
+      truthy = false;
+    }
+  }
+  return truthy;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2);
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1}  === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"])
