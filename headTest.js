3c6abeb1a26c17d800ab4c51fe347d30a8b65051
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-08 15:44:17 -0800

    Add files via upload

---
+++ headTest.js
@@ -0,0 +1,10 @@
+const head = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+console.log(head([1]));
+console.log(head([]));
