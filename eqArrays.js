ec9e8f45ec3d8659ae7a5548ab7e15a7cb4bbcb8
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-08 16:06:26 -0800

    Add files via upload

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,5 @@
+/*
+// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
@@ -5,6 +7,8 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+*/
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -22,10 +26,5 @@ const eqArrays = function(arr1, arr2) {
   return truthy;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
