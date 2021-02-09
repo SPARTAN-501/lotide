962d9d8962b683215d69d049dc2d8ec2a3b1c466
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-08 16:52:48 -0800

    Add files via upload

--- headTest.js
+++ headTest.js
@@ -1,11 +1,17 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  }); 
+  it("returns 'Hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+});
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([1]));
-console.log(head([]));
