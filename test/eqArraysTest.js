34b54d8ea6a64c77141dcc928ab9d1771394c8c3
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-08 23:06:44 -0800

    Add files via upload

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,5 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
---
+++ test/tailTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+// TEST CODE
